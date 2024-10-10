851efb5758dd3836eb7299ec3dab47b0c4f630b6
cyj <cyj@cyjdeMac-mini.local>
2024-10-10 21:58:49 +0800

    v2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # OneJs
+An open source lib that pack ECMAScript Application.
+## Requirements
+``` 
+Rust // Only for Compile
+NodeJS 
+System WebView
+// crates
+msgbox // author: Jang Ryeol, license: MIT
+asar // author: Lucy <lucy@absolucy.moe>, licenses: Apache-2.0 or MIT
+webview-nodejs // author: Winterreisender, license: MIT
+``` 
+## Mode
+### Mode 1
+Version 1.0.0 only support Mode 1.
+The project struct of this mode likes:
+```
+/
+    /app
+    -main
+```
+### Mode 2
+```
+/
+    -.app
+    -app.asar
+    -main
+```
+## Attention
+You should not save user data in `app`,`.app` ,`app.asar`.
+## Usage
+After git clone,
+```
+cd app_v1/src
+```
+and Use your code instead of it. Then,
+```
+cd ..
+asar pack . ../app.asar
+cd ..
+cargo build
+cp target/debug/onejs .
+``` 
+Then, rename onejs.
+Finally , for an basic application, you only need `app.asar` ,`onejs`. 
+## Update
+### Version 2
+Rename app to app_v1
+Add Mode 2
+
+## LICENSE
+MIT
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate msgbox;
 use msgbox::IconType;
-use std::{env::*, process::Command};
+use std::{env::*, process::Command,fs};
+use std::path::Path;
+use asar::AsarReader;
 fn main() {
+    let mut runMode="nopack"; // "nopack"(mode 1),"asar"(mode 2),default is nopack
+
     let mut cmd=Command::new("node");
     let nodeVersionOutputResult=cmd.args(["--version",">log.txt"]).output();
     match nodeVersionOutputResult{
@@ -20,7 +24,50 @@ fn main() {
                             basePath+="/";
                         }
                     }
-                    let nodeSrcRootPath=String::from(basePath+"/app/main.js");
+
+                    // 确定运行模式，若有app.asar则更改为相应的运行模式，否则按照默认的nopack模式运行，不进行处理
+                    if(Path::new((basePath.clone()+"/app.asar").as_str()).exists()){
+                        runMode="asar";
+                    }
+
+                    if(runMode=="asar"){
+                        // 将app.asar解压至当前目录（即basePath）的.app目录下
+                        // 如果.app目录已存在，当作已经解压不处理
+                        let bp=basePath.clone()+"/.app";
+                        let asarExtPath=Path::new(bp.as_str());
+                        if(!asarExtPath.exists()){
+                            // 解压
+                            fs::create_dir(asarExtPath);
+                            let appAsarFile=fs::read(basePath.clone()+"/app.asar").unwrap();
+                            let asarFileReader=AsarReader::new(&appAsarFile,None).unwrap();
+                            // 创建目录
+                            for file in asarFileReader.directories().keys(){
+                                let extDirPathStr=bp.clone()+"/"+file.to_str().unwrap();
+                                let extDirPath=Path::new(extDirPathStr.as_str());
+                                if(!extDirPath.exists()){
+                                    fs::create_dir(extDirPath);
+                                }
+                            }
+                            // 创建文件
+                            for file in asarFileReader.files().keys(){
+                                let extFilePathStr=bp.clone()+"/"+file.to_str().unwrap();
+                                let extFilePath=Path::new(extFilePathStr.as_str());
+                                if(!extFilePath.exists()){
+                                    // 将file复制到extFilePath
+                                    let fileData=asarFileReader.read(file).unwrap().data();
+                                    fs::write(extFilePath,fileData);
+                                }
+                            }
+                            //asarFileReader.
+                            //return;
+                        }
+                    }
+
+                    let mut appMenu="/app";
+                    if(runMode=="asar"){
+                        appMenu="/.app";
+                    }
+                    let nodeSrcRootPath=String::from(basePath+appMenu+"/main.js");
                     println!("Run : {}",nodeSrcRootPath);
                     let mut cmd2=Command::new("node");
                     cmd2.args([nodeSrcRootPath]).output().expect("failed");
